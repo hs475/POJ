de2ed58a591e8247d4457aaa26b9005664760d48
hs475 <106378766+hs475@users.noreply.github.com>
2023-11-11 22:41:48 +0800

    Update poj1650_integer_approximation.cpp

--- poj1650_integer_approximation.cpp
+++ poj1650_integer_approximation.cpp
@@ -1,12 +1,10 @@
-//It is like a race, a1, a2 take turns to add 1.
-
 #include <stdio.h>
 #include <math.h>
 
 void race(double a, int L) {
     int a1 = 1, a2 = 1, res1, res2;
     double e, eabs = 10, tmp;
-    while (a1!=L&&a2!=L) {
+    while (a1!=L+1&&a2!=L+1) {
         e = a - double(a1) / double(a2);
         tmp = fabs(e);
         if (tmp<eabs) {
