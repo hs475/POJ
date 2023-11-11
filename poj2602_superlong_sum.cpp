b1f495e1246c9495871e0637ff274daf5c5f3668
hs475 <106378766+hs475@users.noreply.github.com>
2023-11-11 23:29:54 +0800

    Update poj2602_superlong_sum.cpp

--- poj2602_superlong_sum.cpp
+++ poj2602_superlong_sum.cpp
@@ -1,29 +1,34 @@
+//use putchar, faster than scanf and printf
+
 #include <stdio.h>
 #include <iostream>
 
-const int N = 1e6;
-int a1[N], a2[N];
+const int N = 1e6 + 1;
+char a1[N], a2[N], a[N];
 
 void sum() {
     int tmp, tmpn;
-    for (int i=N-1; i>=a1[0]; --i) {
-        a2[i] += a1[i];
-        tmp = i;
-        while (a2[tmp]>9) {
-            tmpn = a2[tmp];
-            a2[tmp]  = tmpn % 10;
-            a2[--tmp] += tmpn / 10;
+    for (int i=N-1; i>=tmp; --i) {
+		a[i] += a1[i] + a2[i] - '0';
+        if (a[i]>'9') {
+            a[i-1] += 1;
+            a[i] -= 10;
         }
     }
-    while (a2[a2[0]-1]) a2[0]--;
+    while (a[tmp-1]!='0') tmp--;
 }
 
 int main() {
-    int num, a, b;
+    int num, b, c, tmp;
     scanf("%d", &num);
-    a1[0] = N - num;
-    a2[0] = N - num;
-    for (int i=0; i<num; ++i) scanf("%d%d", &a1[N-num+i], &a2[N-num+i]);
+    tmp = N - num;
+	getchar();
+    for (int i=0; i<num; ++i) {
+        a1[tmp+i] = getchar();
+		getchar();
+		a2[tmp+i] = getchar();
+		getchar();
+    }
     sum();
-    for (int i=a2[0]; i<N; ++i) printf("%d", a2[i]);
+    for (int i=tmp; i<N; ++i) putchar(a[i]);
 }
