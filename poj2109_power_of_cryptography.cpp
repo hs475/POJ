a02d51ed4892d60420d57dd3047b65734e236e49
hs475 <106378766+hs475@users.noreply.github.com>
2023-11-13 20:02:39 +0800

    Update poj2109_power_of_cryptography.cpp

--- poj2109_power_of_cryptography.cpp
+++ poj2109_power_of_cryptography.cpp
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include <iostream>
 #include <cmath>
 
-int main()
-{
-	double n,p;
-	while(scanf("%lf%lf", &n, &p)==2) printf("%.0lf\n",pow(p,1.0/n));
+const double eps = 1e-6;
+
+int main() {
+	double n, p;
+	while(scanf("%lf %lf",&n,&p)!=EOF) printf("%.0lf\n", pow(p + eps, 1.0 / n));
 }
