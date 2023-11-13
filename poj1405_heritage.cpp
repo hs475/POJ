96b58b36b87e02acfecb52f321aebeac1d94056f
hs475 <106378766+hs475@users.noreply.github.com>
2023-11-14 01:48:31 +0800

    Update poj1405_heritage.cpp

--- poj1405_heritage.cpp
+++ poj1405_heritage.cpp
@@ -5,54 +5,50 @@
 //7   remain 1/42
 //you can see the rule, remainder is a, the next number is a+1, the next remainder 
 //is 1/a*(a+1)
-//use large base : for example 10000
+//use large base : for example 1000
 
 #include <stdio.h>
 #include <cstring>
 
-const int N = 50001;
-int base = 100000, rest1[N], rest2[N], res[N]; 
+const int N = 30001;
+int base = 1000, rest1[N], res[20][N] = {0}; 
 
-void mul(int base, int s[], int t[]) {
+void mul(int base, int a) {
 	int tmp;
-	memset(res, 0, sizeof(res));
-    res[0] = N - 1;
-	for (int j=N-1; j>=t[0]; j--) {
-		for (int i=N-1; i>=s[0]; i--) {
-			res[i-N+1+j] += s[i] * t[j];
-			tmp = i-N+1+j;
-			while (res[tmp]>base-1) {
-				res[tmp-1] += res[tmp] / base;
-				res[tmp] = res[tmp] % base;
-				tmp--;
-			}
-			if (res[0]>tmp) res[0] = tmp;	
+    res[a][0] = 2 * res[a-1][0] - N;
+	for (int j=N-1; j>=res[a-1][0]; j--) {
+		for (int i=N-1; i>=res[a-1][0]; i--) {
+			res[a][i-N+1+j] += res[a-1][i] * res[a-1][j];
 		}
-	}	
+		res[a][j] += res[a-1][j];
+	}
+	tmp = N - 1;
+	for (int tmp=N-1; tmp>=res[a][0]; --tmp) {
+		if (res[a][tmp]>base-1) {
+			res[a][tmp-1] += res[a][tmp] / base;
+			res[a][tmp] %= base;
+		}
+	}
+	while (!res[a][res[a][0]]) res[a][0]++;
 }
 
 int main() {
     int num, tmp; 
-    rest1[N-1] = 2;
-    rest1[0] = N - 1;
     scanf("%d", &num);
     printf("%d\n", 2);
-    memcpy(res, rest1, sizeof(res));
-    for (int i=1; i<num; ++i) {
-        memcpy(rest2, rest1, sizeof(rest1));
-        rest2[N-1]++;
-        tmp = N - 1;
-        while (rest2[tmp]>base-1) {
-            rest2[tmp] = 0;
-            rest2[tmp-1] += 1;
-            tmp--;
-        }
-        while (rest2[rest2[0]-1]!=0) rest2[0]--; 
-        mul(base, rest1, rest2);
-        printf("%d", rest2[rest2[0]]);
-        for (int i=rest2[0]+1; i<N; ++i) printf("%04d", rest2[i]);
+	printf("%d\n", 3);
+	printf("%d\n", 7);
+	printf("%d\n", 43);
+	res[5][0] = N - 2;
+	res[5][N-2] = 1;
+	res[5][N-1] = 806;
+    for (int i=6; i<=num+1; ++i) {
+        mul(base, i);
+		res[i-1][N-1] ++;
+		printf("%d", res[i-1][res[i-1][0]]);
+        for (int j=res[i-1][0]+1; j<N; ++j) {
+			printf("%03d", res[i-1][j]);
+		}
         printf("\n");
-        memcpy(rest1, res, sizeof(rest1));
     }
 }
-
