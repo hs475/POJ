8716c84b60ab22b0516e2cbb4423da63df17e86d
hs475 <106378766+hs475@users.noreply.github.com>
2024-02-20 16:40:20 +0800

    Add files via upload

---
+++ poj1969_Count_on_Canton.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <stdio.h>
+
+void solve(int n) {
+    int k = (int(pow(n * 8 + 1, 0.5)) - 1) / 2;
+
+    int tmp = n - ((k + 1) * k / 2);
+    if (tmp!=0) {
+        if (k%2) printf("TERM %d IS %d/%d\n", n, tmp, k + 2 - tmp);
+        else printf("TERM %d IS %d/%d\n", n, k + 2 - tmp, tmp);
+    }
+    else {
+        if (k%2) printf("TERM %d IS %d/%d\n", n, 1, k);
+        else printf("TERM %d IS %d/%d\n", n, k, 1);
+    }
+}
+
+int main() {
+    int n;
+    while (scanf("%d", &n)!=EOF) {
+        solve(n);
+    }
+}
